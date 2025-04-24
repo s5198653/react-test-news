a8c94cc2dbda6fd59399d7f6f58c489ff0bf3025
Taranovskaya <s5198653@yandex.ru>
2025-04-24 18:07:12 +0300

    improves CardHeader component

--- src/app/components/CardHeader/CardHeader.tsx
+++ src/app/components/CardHeader/CardHeader.tsx
@@ -8,99 +8,104 @@ import { formatDate } from '@/app/helpers/formatDate/formatDate';
 import { formatCountry } from '@/app/helpers/formatCountry/formatCountry';
 import { formatReach } from '@/app/helpers/formatReach/formatReach';
 import styles from './CardHeader.module.css';
-import type { IData_SnippetNews } from '@/app/types/data';
+import type { IData_DuplicateNews, IData_SnippetNews } from '@/app/types/data';
 import { capitalize } from '@/app/helpers/capitalize/capitalize';
 
 interface CardHeaderProps {
-  news: IData_SnippetNews;
+  news: IData_SnippetNews | IData_DuplicateNews;
   onChange: () => void;
 }
 
 const { Text, Link, Title } = Typography;
 
-export const CardHeader: FC<CardHeaderProps> = ({
-  news: { TI, DP, REACH, DOM, LANG, CNTR, AU, TRAFFIC, URL, CNTR_CODE, SENT },
-  onChange,
-}) => {
+export const CardHeader: FC<CardHeaderProps> = ({ news, onChange }) => {
   return (
-    <Space direction="vertical" size="small" className={styles.card__header}>
+    <Space direction="vertical" size="small" className={styles.cardHeader}>
       <Flex justify="space-between" gap={20}>
         <Space size={10} wrap>
           <Flex gap={2}>
             <Text style={{ color: 'var(--secondary)', fontWeight: 'bold' }}>
-              {formatDate(DP)[0]}
+              {formatDate(news.DP)[0]}
             </Text>
             <Text style={{ color: 'var(--secondary)' }}>
-              {formatDate(DP).slice(1).join(' ')}
+              {formatDate(news.DP).slice(1).join(' ')}
             </Text>
           </Flex>
           <Flex gap={2}>
             <Text style={{ color: 'var(--secondary)', fontWeight: 'bold' }}>
-              {formatReach(REACH)}
+              {formatReach(news.REACH)}
             </Text>
             <Text style={{ color: 'var(--secondary)' }}>Reach</Text>
           </Flex>
-          <Flex gap={2}>
-            <Text style={{ color: 'var(--secondary)' }}>Top Traffic: </Text>
-            {TRAFFIC.map(({ value, count }) => (
-              <div key={value}>
-                <Text style={{ color: 'var(--secondary)' }}>
-                  {formatCountry(value) + ' '}
-                </Text>
-                <Text
-                  style={{
-                    color: 'var(--secondary)',
-                    fontWeight: 'bold',
-                  }}>{`${Math.round(count * 100)}%`}</Text>
-              </div>
-            ))}
-          </Flex>
+          {'TRAFFIC' in news && (
+            <Flex gap={2}>
+              <Text style={{ color: 'var(--secondary)' }}>Top Traffic: </Text>
+              {news.TRAFFIC.map(({ value, count }) => (
+                <div key={value}>
+                  <Text style={{ color: 'var(--secondary)' }}>
+                    {formatCountry(value) + ' '}
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'var(--secondary)',
+                      fontWeight: 'bold',
+                    }}>{`${Math.round(count * 100)}%`}</Text>
+                </div>
+              ))}
+            </Flex>
+          )}
         </Space>
         <Flex gap={10} align="center">
-          <Tag
-            className={`${styles.card__header__sent} ${
-              styles[`card__header__sent__${SENT}`]
-            }`}>
-            {capitalize(SENT)}
-          </Tag>
-          <Tooltip placement="bottom" title={URL}>
-            <Button className={styles.card__header__info}>i</Button>
+          {'SENT' in news && (
+            <Tag
+              className={`${styles.cardHeader__sent} ${
+                styles[`cardHeader__sent__${news.SENT}`]
+              }`}>
+              {capitalize(news.SENT)}
+            </Tag>
+          )}
+          <Tooltip placement="bottom" title="some information">
+            <Button className={styles.cardHeader__info}>i</Button>
           </Tooltip>
           <Checkbox
-            className={styles.card__header__checkbox}
+            className={styles.cardHeader__checkbox}
             onChange={onChange}
           />
         </Flex>
       </Flex>
 
-      <Link href={TI} target="_blank">
+      <Link href={news.TI} target="_blank">
         <Title
           level={2}
           style={{ color: 'var(--general)', margin: 0, fontSize: 20 }}>
-          {TI}
+          {news.TI}
         </Title>
       </Link>
 
       <Space size={10} wrap>
         <Flex gap={3} align="center">
           <GlobalOutlined />
-          <Link>{DOM}</Link>
+          <Link>{news.DOM}</Link>
         </Flex>
 
         <Flex gap={3} align="center">
-          <Flag code={CNTR_CODE.toUpperCase()} height="10" />
-          <Text style={{ color: 'var(--secondary)' }}>{CNTR}</Text>
+          <Flag code={news.CNTR_CODE.toUpperCase()} height="10" />
+          <Text style={{ color: 'var(--secondary)' }}>{news.CNTR}</Text>
         </Flex>
-        <Flex gap={3} align="center">
-          <IoBookOutline />
-          <Text style={{ color: 'var(--secondary)' }}>{LANG}</Text>
-        </Flex>
+        {'LANG' in news && (
+          <Flex gap={3} align="center">
+            <IoBookOutline />
+            <Text style={{ color: 'var(--secondary)' }}>{news.LANG}</Text>
+          </Flex>
+        )}
 
-        {AU.length > 0 && (
+        {news.AU.length > 0 && (
           <Flex gap={2}>
             <UserOutlined />
             <Text style={{ color: 'var(--secondary)' }}>
-              {AU.length > 1 ? `${AU.slice(1).join(', ')}, et al.` : AU[0]}
+              {news.AU.length > 1
+                ? `${news.AU.slice(1).join(', ')}, et al.`
+                : news.AU[0]}
             </Text>
           </Flex>
         )}
